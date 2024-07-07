63fb29c53e1b75925e9974b0365616194fac673e
akshaysingh6394 <singhakshay8081@gmail.com>
2024-07-08 00:29:31 +0530

    create

---
+++ Basic/create.java
@@ -0,0 +1,14 @@
+package Basic;
+import java.util.*;
+public class create {
+    public static void main(String[] args){
+        int marks[] = new int[50];
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+        System.out.println("phy"+" "+marks[0]);
+        System.out.println("chem"+" "+marks[0]);
+        System.out.println("math"+" "+marks[0]);
+    }
+}
