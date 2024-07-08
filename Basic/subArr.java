7b2e1b06f6f2b6bef7f9a9965ed6be7cf4bfcb62
akshaysingh6394 <singhakshay8081@gmail.com>
2024-07-08 23:40:06 +0530

    right code now

--- Basic/subArr.java
+++ Basic/subArr.java
@@ -6,11 +6,12 @@ public class subArr {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
                 
-                for (int k = i; k < j; k++) {
-                    System.out.print(arr[k]);
-                    ts++;
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k]+" ");
+                    
                     
                 }
+                ts++;
                 System.out.println();
             }
             System.out.println();
@@ -18,7 +19,7 @@ public class subArr {
         System.out.println(ts);
     }
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
+        int arr[] = {1,2,3,4,5};
         subArray(arr);
     }
     
