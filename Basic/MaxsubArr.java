c2be5b50168c52564e6dffe3045c1d130679b60f
akshaysingh6394 <singhakshay8081@gmail.com>
2024-07-08 23:32:14 +0530

    maxSUbArray

---
+++ Basic/MaxsubArr.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class MaxsubArr {
+    public static void maxSubArray(int arr[]){
+        int currSum =0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k < j; k++) {
+                    currSum += arr[k];
+                    
+                }
+                if(currSum>maxSum){
+                    maxSum=currSum;
+                }
+            }
+            
+        }
+        System.out.println(maxSum);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        maxSubArray(arr);
+    }
+    
+}
