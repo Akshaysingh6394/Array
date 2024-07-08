9713982be149646699dc2f6b77ab09033063a013
akshaysingh6394 <singhakshay8081@gmail.com>
2024-07-08 15:33:11 +0530

    Reverse

---
+++ Basic/reverse.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class reverse {
+    public static void reverseArray(int arr[]){
+        int start =0 , end = arr.length-1;
+        while(start<end){
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        reverseArray(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
